b99afeed1850699697391e02b09cd5d6a70547ff
JOVEMMMM <jv058450@gmail.com>
2026-03-26 22:43:46 -0300

    Add Exemplar and Livro classes with methods

--- Classe.java
+++ Classe.java
@@ -37,3 +37,42 @@ class Professor extends Usuario {
         return 0.0; [span_17](start_span)// Professores não pagam multa[span_17](end_span)
     }
 }
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Exemplar {
+    private String codigoUnico;
+    private String status; [span_18](start_span)// "disponível" ou "emprestado"[span_18](end_span)
+
+    public Exemplar(String codigoUnico) {
+        this.codigoUnico = codigoUnico;
+        this.status = "disponível";
+    }
+
+    // Getters e Setters (Encapsulamento)
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
+
+class Livro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private List<Exemplar> exemplares; [span_19](start_span)// Composição[span_19](end_span)
+
+    public Livro(String isbn, String titulo, String autor, String editora) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.exemplares = new ArrayList<>();
+    }
+
+    public void adicionarExemplar(String codigo) {
+        exemplares.add(new Exemplar(codigo));
+    }
+}
+
+
